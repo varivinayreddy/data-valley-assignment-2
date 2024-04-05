2ce8979ba57277e93fd4d7d851813e44c641f03e
varivinayreddy <128284140+varivinayreddy@users.noreply.github.com>
2024-04-05 13:04:09 +0530

    Create SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,14 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 200, b = 300;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        swapNumbers(a, b);
+    }
+
+    public static void swapNumbers(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
